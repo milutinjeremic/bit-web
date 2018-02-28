d66102444d915bcc8c719d76820cd42e0721e291
BIT Student <classroom@bgit.rs>
2018-02-28 13:02:08 +0100

    DOM exercises

--- w5/d2/index.js
+++ w5/d2/index.js
@@ -1,4 +1,24 @@
-var ul = document.getElementsByClassName("active");
+var ul = document.getElementsByClassName("active")[0].parentElement.parentElement.previousElementSibling.firstElementChild.firstElementChild;
+var b = document.getElementsByClassName('active')[0];
+
+ul.className="active";
+b.className = " ";
+
+
+
+function test1 (){
+
+  var x =  document.getElementsByClassName("selected")[0];
+  
+  
+  var y = x.parentElement.nextElementSibling.firstElementChild.nextElementSibling;
+  y.className="selected";
+  x.className="";
+
+}
+
+
+
 
 
 
