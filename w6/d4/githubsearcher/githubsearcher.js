a3293e3ed07fd337143a5c8449bdd13e66f5c036
milutinjeremic <milutin.jeremic@gmail.com>
2018-03-08 22:22:07 +0100

    api try

---
+++ w6/d4/githubsearcher/githubsearcher.js
@@ -0,0 +1,35 @@
+
+$("#search").on("keydown", function (event) {
+    
+    if (event.keyCode == 13) {
+        event.preventDefault();
+        
+    }
+});
+
+var user = $("#search").val();
+
+    var request = $.ajax({
+        url: "https://api.github.com/search/users?q="+user,
+        method: "GET"
+    });
+    request.done(function (response) {
+        const users = response.items;
+
+        console.log(users);
+
+        //NEMAM POJMA ?
+        for (let i = 0; i < users.length; i++) {
+            const img = $(`<img src="${users[i].avatar_url}" />`)
+            $("#test").append(img);
+            const login = $(`<p>${users[i].login}<p/>`)
+            $("#test").append(login);
+        }
+
+    });
+
+    request.fail(function (jqXHR, textStatus) {
+        alert("Request failed" + textStatus);
+    })
+
+  
